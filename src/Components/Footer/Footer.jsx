51a9681d9c2d59e17f06c9d258e919bff755d695
macdasy <macdasys@gmail.com>
2023-04-22 12:52:00 +0530

    footer added

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -3,50 +3,99 @@ import Grid from '@mui/material/Grid';
 import { Facebook, Instagram, WhatsApp } from '@mui/icons-material';
 import IconButton from '@mui/material/IconButton';
 
-import logo from '../../Stacks/logo1.png';
+import logo from '../../Stacks/logo.png';
+import './footer.css';
 
 export default function Footer(props){
 
     return(
-        <div id='modal-footer' style={{ padding:'5% 0% 0%' }} >
-                  <hr style={{marginBottom:'3%'}} />
-                <Grid container rowSpacing={5} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-                  <Grid item xs={12} md={4} style={{  paddingLeft:'7%' }}>
-                    <h4 style={{ fontFamily:'Josefin Sans', color:'#7d875f' }}> Urja </h4> 
-                    <div style={{lineHeight:2}} id='atagcont' >
-                      <a href='./collections'> Collections </a> <br/>
-                      <a href='./about'> About </a> <br/>
-                      <a href='./contact'> Contact </a> <br/>
-                      <a href='./policy'> Privacy Policy </a> <br/>
-                    </div>
-                  </Grid>
+        <div id='footer'>
+              <div id='joinus' style={{textAlign:'center', marginBottom:"9%"}} >
+                <h2 style={{ fontFamily:'Schibsted Grotesk', marginBottom:'1%'}}> Admire the world with Urja </h2>
+                <h4 style={{ marginTop:0, marginBottom:'4%' }} > Join us and start attracting! </h4>
+                <button className='uk-button uk-button-secondary' style={{ textTransform:'none', fontFamily:'Schibsted Grotesk' }}> Shop Now </button>
+              </div>
 
-                  <Grid item xs={12} md={4} style={{ paddingLeft:'7%' }}>
-                    <h4 style={{ fontFamily:'Josefin Sans', color:'#7d875f' }}> Shop Details </h4> 
-                    <div id='addcont'>
-                        <p> 12/24 </p>
-                        <p> Vascodagam Street, </p>
-                        <p> Jillianh, </p>
-                        <p> Bangalore, </p>
-                        <IconButton style={{paddingLeft:0, background:'none'}} >
-                          <Instagram style={{ color: '#7d875f' }} />
-                        </IconButton>
-                        <IconButton style={{ background:'none'}}>
-                          <Facebook style={{ color: '#7d875f' }} />                        
-                        </IconButton>
-                        <IconButton style={{ background:'none'}}>
-                          <WhatsApp style={{ color: '#7d875f' }} />                    
-                        </IconButton>
+              <div id='foo-sec'>
+                <div class="uk-child-width-1-2@s uk-child-width-1-5@m uk-text-center" data-uk-grid 
+                  style={{borderTop:'1px solid white', borderBottom:'1px solid white', padding:'3% 8%', margin:0}}>
+                    
+                    <div>
+                      <div class="foo-link-sec uk-width-1-2@s uk-width-2-5@m">
+                          <ul class="uk-nav uk-nav-default">
+                              <li class="uk-active"><a href="#"> Links </a></li>
+                              <li><a href="."> Home </a></li>
+                              <li><a href="/collections"> Collections </a></li>
+                              <li><a href="/cart"> Cart </a></li>
+                          </ul>
                       </div>
-                  </Grid>
+                    </div>
 
-                  <Grid item xs={12} md={4}  style={{ textAlign:'center' }}>
-                    <img src={logo} alt='' style={{ width:"60%" }}/>
-                  </Grid>
-                </Grid>
-                <hr style={{marginBottom:'3%'}} />
+                    <div>
+                      <div class="foo-link-sec uk-width-1-2@s uk-width-2-5@m">
+                          <ul class="uk-nav uk-nav-default">
+                              <li class="uk-active"><a href="#"> Company </a></li>
+                              <li><a href="#"> About </a></li>
+                              <li><a href="#"> Founder's Note </a></li>
+                          </ul>
+                      </div>
+                    </div>
 
+                    <div>
+                      <div class="foo-link-sec uk-width-1-2@s uk-width-2-5@m">
+                          <ul class="uk-nav uk-nav-default">
+                              <li class="uk-active"><a href="#">Policy</a></li>
+                              <li><a href="#"> Return & Refund </a></li>
+                              <li><a href="#"> Shipping Policy </a></li>
+                              <li><a href="#"> Privacy Policy </a></li>
+                          </ul>
+                      </div>
+                    </div>
+
+                    <div>
+                      <div class="foo-link-sec uk-width-1-2@s uk-width-2-5@m">
+                          <ul class="uk-nav uk-nav-default">
+                              <li class="uk-active"><a href="#"> Important Links </a></li>
+                              <li><a href="#"> Term & Conditions </a></li>
+                              <li><a href="#"> Wash & Care Instruction </a></li>
+                          </ul>
+                      </div>
+                    </div>
+
+                    <div>
+                      <div class="foo-link-sec uk-width-1-2@s uk-width-2-5@m">
+                          <ul class="uk-nav uk-nav-default">
+                              <li class="uk-active"><a href="#"> Support </a></li>
+                              <li><a href="#"> Contact </a></li>
+                              <li><a href="#"> Forum </a></li>
+                          </ul>
+                        </div>                      
+                    </div>
                 </div>
+
+                <div id='foo-contact'>
+                  <div class="foo-link-sec uk-child-width-1-2@s uk-text-center" data-uk-grid>
+                    <ul class="uk-nav uk-nav-default" style={{textAlign:'center'}}>
+                      <li class="uk-active"><a href="#" className='foo-mail'> Urja - The Dying Studio </a></li>
+                      <li><a href="#" className='foo-mail-1'> urja@gmail.com </a></li>
+                    </ul>
+
+                    <div id='foo-icon-sec' style={{paddingRight:"5%"}} >
+                        <a href="" class="uk-icon-button uk-margin-small-right" uk-icon="twitter"></a>
+                        <a href="" class="uk-icon-button uk-margin-small-right" uk-icon="facebook"></a>
+                        <a href="" class="uk-icon-button uk-margin-small-right" uk-icon="instagram"></a>
+                    </div>
+                  </div>
+                </div>
+
+                <div style={{textAlign:'center', padding:'1%'}}>
+                    <p style={{ fontSize:"small", marginBottom:0, color:'grey', fontFamily:'Schibsted Grotesk'}}> &#169;2023, Urja - The Dying Studio. </p>
+                </div>
+
+
+              </div>
+                
+        </div>
     )
 
 }
